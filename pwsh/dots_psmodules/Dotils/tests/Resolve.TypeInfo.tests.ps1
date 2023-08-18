a135a4617ed0490354e5ebc3c5266c7f6b740525
Jake Bolton <ninmonkeys@gmail.com>
2023-08-18 18:06:48 -0500

    Update Resolve.TypeInfo.tests.ps1

--- pwsh/dots_psmodules/Dotils/tests/Resolve.TypeInfo.tests.ps1
+++ pwsh/dots_psmodules/Dotils/tests/Resolve.TypeInfo.tests.ps1
@@ -1,6 +1,8 @@
 BeforeAll {
     Import-Module -ea 'stop' 'Dotils' -force -passthru  | write-host
+    hr  | write-host
 }
+
 Describe 'Dotils.Test-IsOfType.FancyWip' {
     it 'sample as strings' {
         'red'
@@ -29,5 +31,73 @@ Describe 'Resolve.TypeInfo' -tags 'NeedsMoreT4ests' {
 
         'red' -is [System.ConsoleColor]
         'red' -as 'ConsoleColor' -is [ConsoleColor]
+        __compare-Is.Type $Null -TypeName 'IO.DirectoryInfo' -Debug
     #>
 }
+Describe '__compare-Is.Type'  {
+    it 'null with -AllowNull should not throw' {
+        { __compare-Is.Type $Null -TypeName 'IO.DirectoryInfo' -AllowNull:$true }
+            | Should -not -Throw
+    }
+    it 'null should throw' {
+        { __compare-Is.Type $Null -TypeName 'IO.DirectoryInfo' -AllowNull:$false }
+            | Should -Throw
+    }
+    it ' Allow Nulls: <Left> is <Right>' -foreach @(
+
+        @{
+            Left = $null
+            Right = 'IO.DirectoryInfo'
+            ExpectedValue = $false
+            AllowNull = $true
+        }
+    ) {
+        __compare-Is.Type $Null -TypeName 'IO.DirectoryInfo' -Debug
+        $test = __compare-Is.Type -InputObject $Left -TypeName $Right -AllowNull
+        $test | Should -BeOfType 'bool'
+        $test | Should -BeExactly $ExpectedValue
+
+    }
+    it ' <Left> is <Right>' -foreach @(
+        @{
+            Left = (gi .).GetType()
+            Right = 'IO.DirectoryInfo'
+            ExpectedValue = $True
+        }
+    ) {
+        $test = __compare-Is.Type -InputObject $Left -TypeName $Right
+        $test | Should -BeOfType 'bool'
+        $test | Should -BeExactly $ExpectedValue
+    }
+    it -pending ' <Left> is <Right> is <ExpectedValue>' -foreach @(
+        @{
+            Left = 2
+            Right = 'int'
+            ExpectedValue = $true
+            # CompareString = $True
+        }
+        @{
+            Left = 2
+            Right = 'int'
+            ExpectedValue = $false
+            # CompareString = $false
+        }
+        @{
+            Left = 2.2
+            Right = 'int'
+            ExpectedValue = $false
+            # CompareString = $false
+        }
+    ) {
+        $query = dotils\__compare-Is.Type -InputObject $Left -TypeName $Right # -CompareString:$CompareString
+        $query | Should -BeOfType 'bool' -because 'logical test'
+        $query | Should -Be $ExpectedValue
+    }
+    <#
+
+        'red' -is [System.ConsoleColor]
+        'red' -as 'ConsoleColor' -is [ConsoleColor]
+    #>
+}
+
+
